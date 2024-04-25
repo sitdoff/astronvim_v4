0533f2e0ff1766e22e0c8588c917fbf6e3327590
Roman Ivanov <sitdoff@gmail.com>
2024-04-25 09:45:10 +0300

    update luasnip config

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,3 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -16,3 +14,20 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.html" },
+  command = "set filetype=htmldjango",
+})
+vim.api.nvim_create_augroup("neotree_autoopen", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  desc = "Open Neotree on enter",
+  group = "neotree_autoopen",
+  once = true,
+  callback = function()
+    if not vim.g.neotree_opened then
+      vim.cmd "Neotree focus"
+      vim.g.neotree_opened = true
+    end
+  end,
+})
+require("luasnip").filetype_extend("htmldjango", { "html" })
