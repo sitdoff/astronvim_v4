9e8b52e4f58ff330d5fc874cd1138703bf273c87
Roman Ivanov <sitdoff@gmail.com>
2024-11-29 14:47:23 +0300

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,26 +2,26 @@
 
 ---@type LazySpec
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    "nvimtools/none-ls.nvim",
+    opts = function(_, config)
+        -- config variable is the default configuration table for the setup function call
+        -- local null_ls = require "null-ls"
 
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
-      -- null_ls.builtins.formatting.prettierd,
-      -- null_ls.builtins.diagnostics.flake8,
-      -- null_ls.builtins.diagnostics.djlint,
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.black,
-      -- null_ls.builtins.formatting.isort,
-      -- null_ls.builtins.formatting.djlint,
-    }
-    return config -- return final config table
-  end,
+        -- Check supported formatters and linters
+        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+        -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+        config.sources = {
+            -- Set a formatter
+            -- null_ls.builtins.formatting.stylua,
+            -- null_ls.builtins.formatting.prettier,
+            -- null_ls.builtins.formatting.prettierd,
+            -- null_ls.builtins.diagnostics.flake8,
+            -- null_ls.builtins.diagnostics.djlint,
+            -- null_ls.builtins.formatting.stylua,
+            -- null_ls.builtins.formatting.black,
+            -- null_ls.builtins.formatting.isort,
+            -- null_ls.builtins.formatting.djlint,
+        }
+        return config -- return final config table
+    end,
 }
